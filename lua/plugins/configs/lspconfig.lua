c8e744bb38e29ab0690ee629861e7ed815279d4c
Sohun Patel <sohun.patel@archescomputing.com>
2024-10-21 11:44:38 -0400

    Updates

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -32,7 +32,7 @@ M.capabilites.textDocument.completion.completionItem = {
 
 local lspconfig = require "lspconfig"
 
-local servers = { "clangd", "cmake", "harper_ls", "jinja_lsp", "lua_ls", "ruff", "rust_analyzer", "verible" }
+local servers = { "clangd", "cmake", "jinja_lsp", "lua_ls", "ruff", "rust_analyzer", "verible" }
 
 lspconfig.pylsp.setup {
   settings = {
