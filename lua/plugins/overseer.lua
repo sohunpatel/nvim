c842498237ee86f3aad5032ddad17e86e5ee385a
Sohun Patel <sohun.patel@archescomputing.com>
2025-09-04 01:21:04 -0400

    feat: add custom templates for overseer

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -3,7 +3,8 @@ return {
         "stevearc/overseer.nvim",
 
         config = function()
-            require("overseer").setup({
+            local overseer = require("overseer")
+            overseer.setup({
                 task_list = {
                     direction = "right"
                 }
@@ -13,15 +14,36 @@ return {
             vim.keymap.set("n", "<leader>tr", function()
                 local input = vim.fn.input("Run: ")
                 if input ~= "" then
-                    require("overseer").run_template({ name = input })
+                    overseer.run_template({ name = input })
                 end
             end, { desc = "Overseer: run task" })
             vim.keymap.set("n", "<leader>tl", function()
-                local available = require("overseer").list_tasks({ unique = true, recent_first = true })
+                local available = overseer.list_tasks({ unique = true, recent_first = true })
+                local names = {}
+                for _, task in ipairs(available) do
+                    if task.name then
+                        table.insert(names, task.name)
+                    end
+                end
+                require("fzf-lua").fzf_exec(names)
             end, { desc = "Overseer: list available tasks" })
             vim.keymap.set("n", "<leader>tt", function()
-                require("overseer").toggle({})
+                overseer.toggle({})
             end, { desc = "Overseer: toggle status" })
+            vim.keymap.set("n", "<leader>tj", function()
+                local tasks = overseer.list_tasks({ recent_first = true })
+                if vim.tbl_isempty(tasks) then
+                    vim.notify("No tasks found", vim.log.levels.WARN)
+                else
+                    overseer.run_action(tasks[1], "restart")
+                end
+            end, { desc = "Overseer: restart last action" })
+
+            -- Load my custom templats/providers
+            local providers = require("plugins.custom")
+            for _, provider in ipairs(providers) do
+                overseer.register_template(provider)
+            end
         end
     }
 }
